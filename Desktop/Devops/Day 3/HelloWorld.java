e4b114ecc450374690da2da61b2c2712d1e2a725
venkatesh1988 <venkatesh.se05@gmail.com>
2024-10-07 02:55:35 +0530

    adding_changes

--- Desktop/Devops/Day 3/HelloWorld.java
+++ Desktop/Devops/Day 3/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first java program");
-        System.out.println("This is another branch");
     }
 }
