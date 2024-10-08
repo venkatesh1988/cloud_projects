d92c50bc9f88932bd7243ab81417a4ae8920f21b
venkatesh1988 <venkatesh.se05@gmail.com>
2024-10-08 21:12:53 +0530

    For_Jenkins

--- Desktop/Devops/Day 3/Simple.java
+++ Desktop/Devops/Day 3/Simple.java
@@ -1,6 +1,6 @@
 public class Simple {
     public static void main(String[] args) {
-        System.out.println("This is my first java application build in Jenkins");
+        System.out.println("This is my first java application to be build in Jenkins");
     }
     
 }
