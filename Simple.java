3ff314a7036b74e572fb326ff3933b9b46016a49
VENKATESH D <venkatesh.se05@gmail.com>
2024-10-10 06:39:53 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,7 +1,7 @@
 public class Simple {
     public static void main(String[] args) {
         System.out.println("This is my first java application to be build in Jenkins");
-         System.out.println("Hello...!!");
+         System.out.println("Hello Jenkins Family..!! This is a new update from developer");
     }
     
 }
